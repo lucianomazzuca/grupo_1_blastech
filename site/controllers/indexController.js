263c1a8dcd031c212f52411d551a8990aaef269a
SantiRamallo <67606452+SantiRamallo@users.noreply.github.com>
2020-09-19 08:28:35 -0300

    terminado index dinamico con sus productos

--- site/controllers/indexController.js
+++ site/controllers/indexController.js
@@ -1,14 +1,32 @@
 const path = require('path');
 const dbProducts = require(path.join(__dirname, '..', 'data', 'dbProducts'));
 
+
 module.exports = {
-    home: function(req,res){
-        res.render('index', { 
-            title: 'Blastech',
-            css:'index.css',
+    // home: function(req,res){
+    //     let productos = dbProducts;
+    //     res.render('index', { 
+    //         title: 'Blastech',
+    //         css:'index.css',
+    //     })
+        
+        
+    // },
+    home:function(req,res){
+        let destacados = dbProducts.filter(producto=>{
+            return producto.status == "visited"
         })
         
-        
+        let ofertas = dbProducts.filter(producto=>{
+            return producto.status == "in-sale"
+        })
+
+        res.render('index', { 
+            title: 'Mercado Liebre',
+            css:'index.css',
+            destacados: destacados,
+            ofertas:ofertas
+        });
     },
     email: function (req, res) {
         res.render('email', {
