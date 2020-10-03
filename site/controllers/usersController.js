230fc00c9cd15bf474bd0aeeb4a76a044c6dfc22
SantiRamallo <67606452+SantiRamallo@users.noreply.github.com>
2020-10-03 11:35:11 -0300

    register funcionando, falta arreglar estilos

--- site/controllers/usersController.js
+++ site/controllers/usersController.js
@@ -42,10 +42,46 @@ module.exports = {
     },
     registro: function (req, res) {
         res.render('registro', {
-            title: 'Registrate',
+            title: 'Registro de Usuario',
             css: 'login.css',
         })
     },
+    processRegister:function(req,res){
+        //res.send(req.body)
+        //res.send(req.files)
+        let errors = validationResult(req);
+        let lastID = 0;
+        if(dbUsers.length != 0){
+            dbUsers.forEach(user => {
+                if(user.id > lastID){
+                    lastID = user.id
+                }
+            })
+        }
+        if(errors.isEmpty()){
+            let newUser = {
+                id: lastID + 1,
+                nombre: req.body.nombre.trim(),
+                email: req.body.email.trim(),
+                avatar: (req.files[0])?req.files[0].filename:"default.png",
+                password:bcrypt.hashSync(req.body.pass,10),
+                rol:"user"
+            }
+            dbUsers.push(newUser);
+            fs.writeFileSync(path.join(__dirname,'..','data','dbUsers.json'),JSON.stringify(dbUsers),'utf-8')
+    
+            return res.redirect('/users/login')
+        }else{
+            res.render('userRegister',{
+                title:"Registro de Usuario",
+                css:"index.css",
+                errors:errors.mapped(),
+                old:req.body
+            })
+        }
+       
+    },
+
     restablecer: function (req, res) {
         res.render('restablecer', {
             title: 'Reestablecer contraseña',
--- site/routes/users.js
+++ site/routes/users.js
@@ -4,7 +4,10 @@ var router = express.Router();
 const usersController = require('../controllers/usersController.js');
 const loginValidator = require('../validations/loginValidator');
 
+const upImagesUsers = require('../middlewares/upImagesUsers')
+
 router.get('/registro', usersController.registro);
+router.post('/registro',upImagesUsers.any(), usersController.processRegister);
 
 router.get('/login', usersController.login);
 router.post('/login', loginValidator, usersController.processLogin);
