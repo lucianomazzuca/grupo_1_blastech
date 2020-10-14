d5ceb0c76e913317ef8d6113ead0f45db2df157a
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2020-10-13 23:30:41 -0300

    Arreglado boton filter en listado de productos

---
+++ site/public/javascript/filterDropdown.js
@@ -0,0 +1,8 @@
+const btnFilter = document.querySelector('#btn-filtro');
+const menuFilter = document.querySelector('.filtro-categorias');
+
+btnFilter.addEventListener('click', openMenuFilter);
+
+function openMenuFilter() {
+    menuFilter.classList.toggle('active');
+}
