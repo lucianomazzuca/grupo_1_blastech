b77ea9db80c030a40395cfc589c2090ce4fae639
SantiRamallo <67606452+SantiRamallo@users.noreply.github.com>
2020-10-28 11:41:52 -0300

    arrancandos los modelos

---
+++ site/database/models/Product.js
@@ -0,0 +1,52 @@
+module.exports = (sequelize, dataTypes) => {
+
+    let alias = "Products";
+
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        model: {
+            type: dataTypes.STRING(45),
+            allowNull: false
+        },
+        price: {
+            type: dataTypes.DECIMAL(5, 2).UNSIGNED,
+            allowNull: false
+        },
+        discount: {
+            type: dataTypes.INTEGER,
+            allowNull: false
+        },
+        images: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        },
+        description: {
+            type: dataTypes.STRING(200),
+            allowNull: false
+        },
+        features: {
+            type: dataTypes.STRING(500),
+        },
+        status: {
+            type: dataTypes.STRING(45),
+            allowNull: false
+        }
+    }
+
+    let config = {
+        tableName: "products",
+        timestamps: true,
+        underscored: true
+    }
+
+
+
+    const Products = sequelize.define(alias, cols, config);
+
+    return Products;
+}
