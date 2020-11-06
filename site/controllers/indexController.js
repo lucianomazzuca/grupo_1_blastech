366b4c3898b2f26bf16bfb25a64996fa7d0abc43
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2020-11-06 04:11:40 -0300

    Actualizado home con sql

--- site/controllers/indexController.js
+++ site/controllers/indexController.js
@@ -1,32 +1,64 @@
 const path = require('path');
 const dbProducts = require(path.join(__dirname, '..', 'data', 'dbProducts'));
-
+const db = require("../database/models");
+const sequelize = db.sequelize;
+const { Op } = require("sequelize");
 
 module.exports = {
-    // home: function(req,res){
-    //     let productos = dbProducts;
-    //     res.render('index', { 
-    //         title: 'Blastech',
-    //         css:'index.css',
-    //     })
-        
-        
-    // },
+
     home:function(req,res){
-        let destacados = dbProducts.filter(producto=>{
-            return producto.status == "visited"
-        })
+        // let destacados = dbProducts.filter(producto=>{
+        //     return producto.status == "visited"
+        // })
         
-        let ofertas = dbProducts.filter(producto=>{
-            return producto.status == "in-sale"
+        // let ofertas = dbProducts.filter(producto=>{
+        //     return producto.status == "in-sale"
+        // })
+
+        // res.render('index', { 
+        //     title: 'Blastech',
+        //     css:'index.css',
+        //     destacados: destacados,
+        //     ofertas:ofertas
+        // });
+
+        let destacados = db.Products.findAll({
+            include: [
+                {
+                    association: 'brands',
+                },
+                {
+                    association: 'categories',
+                },
+            ],
+            where: {
+                status: { [Op.eq]: 'destacado' }
+            }
         })
 
-        res.render('index', { 
-            title: 'Blastech',
-            css:'index.css',
-            destacados: destacados,
-            ofertas:ofertas
-        });
+        let ofertas = db.Products.findAll({
+            include: [
+                {
+                    association: 'brands',
+                },
+                {
+                    association: 'categories',
+                },
+            ],
+            where: {
+                status: { [Op.eq]: 'in-sale' }
+            }
+        })
+        
+        Promise.all([destacados, ofertas])
+        .then(([destacados, ofertas]) => {
+            res.render('index', {
+                title: 'Blastech',
+                css:'index.css',
+                destacados: destacados,
+                ofertas:ofertas
+            })
+        })
     },
     email: function (req, res) {
         res.render('email', {
